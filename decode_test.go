08ab940080ca4e47274b393ac28549a0284b12eb
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2020-05-19 17:59:30 -0300

    Decode nil

--- decode_test.go
+++ decode_test.go
@@ -20,3 +20,32 @@ func TestDecodingBool(t *testing.T) {
 		t.Errorf("did not decode booleans propperly")
 	}
 }
+
+func TestDecodingNil(t *testing.T) {
+	b := true
+	i := 123
+	str := "hello, world"
+	arr := []int{1, 2, 3}
+	dict := map[string]int{"1": 1, "2": 2}
+
+	null := []byte{chrNone}
+
+	Decode(null, &b)
+	Decode(null, &i)
+	Decode(null, &str)
+	Decode(null, &arr)
+	Decode(null, &dict)
+
+	if b != false {
+		t.Error("bool is a non-zero value")
+	}
+	if i != 0 {
+		t.Error("int is a non-zero value")
+	}
+	if arr != nil {
+		t.Error("array is a non-zero value")
+	}
+	if dict != nil {
+		t.Error("map is a non-zero value")
+	}
+}
